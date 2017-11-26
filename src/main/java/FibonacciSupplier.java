b74ae53ee6ec5d334f3d201f34dec715f4936efa
644942732@qq.com <644942732>
2017-11-26 12:42:43 +0800

    first change

--- src/main/java/FibonacciSupplier.java
+++ src/main/java/FibonacciSupplier.java
@@ -4,15 +4,17 @@ import java.util.function.Supplier;
  * Created by qiuzhanghua on 2017/2/7.
  */
 public class FibonacciSupplier implements Supplier<Long> {
-  private long previous = 1;
-  private long current = 2;
+	private long previous = 1;
+	private long current = 4;
+	private long previous2=2;
+	@Override
+	public Long get() {
 
-  @Override
-  public Long get() {
-    long p = previous;
-    long next = current + previous;
-    previous = current;
-    current = next;
-    return p;
-  }
+		long p = previous;
+		long next = current + previous+previous2;
+		previous=previous2;
+		previous2 = current;
+		current = next;
+		return p;
+	}
 }
